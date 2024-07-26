93d9a9f64fcb2669823ae4fc5272e149849bd13b
NicolaStorato <10934090@itisrossi.vi.it>
2024-07-26 19:35:05 +0200

    passaggio pagina

--- mainMenuScript.js
+++ mainMenuScript.js
@@ -1,3 +1,17 @@
-document.getElementById("classificaButton").addEventListener("click", function() {
-    window.location.href = 'index.html';
+document.getElementById("inserisciNickname").addEventListener("click", function() {
+    
+    let player1 = document.getElementById("nickname1").value;
+    console.log(player1);
+
+    let player2 = document.getElementById("nickname2").value;
+    console.log(player2);
+
+    if(player1 == 0 || player2 == 0){
+        alert("Inserire i nicknames");
+    }
+
+    if(player1 != 0 && player2 != 0){
+        window.location.href = 'index.html';
+    }
+    
 })
